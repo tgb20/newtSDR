e43ab564f265cb2f6bb8f681dc97e929bcbd17e1
Jacob Bashista <tgb.bashista@gmail.com>
2018-07-02 10:40:27 -0400

    Initial Commit

---
+++ newtSDR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  newtSDR
+//
+//  Created by Jacob Bashista on 7/2/18.
+//  Copyright © 2018 Jacob Bashista. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ newtSDR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  newtSDR
+//
+//  Created by Jacob Bashista on 7/2/18.
+//  Copyright © 2018 Jacob Bashista. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
